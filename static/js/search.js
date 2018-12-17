253cebef1deeee3ebf5c0f271f262a00e286babb
Huang Kun Lun <huang-kun-lun@pacific-textiles.com>
2018-12-17 14:34:06 +0800

    modify search function to native JS

--- static/js/search.js
+++ static/js/search.js
@@ -1,38 +1,64 @@
-$("#tet").click(function(){
-  var html = '';
-  $.ajax({
-	  url: '../index.xml',
-      dataType: 'xml',
-	  success: function (data) {
-		var count = 0;
-		html = '<table class="table table-bordered"><thead><tr><th scope="col" class="mb-2">#</th><th scope="col" class="mb-2">标题</th><th scope="col" class="mb-5">链接</th></tr></thead><tbody>';
-		var keyword = $('#keyword').val().toLowerCase();
-		if (keyword == ""){
-			html = '<h2>No Input!</h2>'
-		}else{
-		   $(data).find('item').each(function () {
-           var $this = $(this);
-		   var judge = $this.find('title').text().toLowerCase();
-           var key = $this.find('title').text();
-		   if(judge.indexOf(keyword)!=-1){
-             $this.find('title').each(function () {
-				html += '<tr><th scope="row">' + count + '</th><td>' + key + '</td><td><a href="' + $this.find('link').text() + '">' + $this.find('link').text() + '</a></td></tr>';
-			   });	
+function loadXMLDoc(url)
+{
+	
+var xmlhttp;
+var html = '';
+var x,xx,i;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+		console.log('xml');
+	var count = 0;
+	html = '<table class="table table-bordered"><thead><tr><th scope="col" class="mb-2">#</th><th scope="col" class="mb-2">标题</th><th scope="col" class="mb-5">链接</th></tr></thead><tbody>';
+	//var keyword = $('#keyword').val().toLowerCase();
+	var keyword = document.querySelector('#keyword').value.toLowerCase();
+	if (keyword == ""){
+			html = '<h2>No Input!</h2>';
+	}else{
+		x=xmlhttp.responseXML.documentElement.getElementsByTagName("item");
+		for (i=0;i<x.length;i++)
+		{
+	      //var judge = x[i].getElementsByTagName("title").toLowerCase();
+		  //var key = x[i].getElementsByTagName("title");
+		  xx=x[i].getElementsByTagName("title");
+		  {
+			try
+			  {
+			  var judge = xx[0].firstChild.nodeValue.toLowerCase();
+			  var key = xx[0].firstChild.nodeValue;
+			  var posturl = x[i].getElementsByTagName("link")[0].firstChild.nodeValue;
+			  }
+			catch (er)
+			  {
+				console.log(er);  
+			  continue;
+			  }
+			}
+		  if(judge.indexOf(keyword)!=-1){
+			  html += '<tr><th scope="row">' + count + '</th><td>' + key + '</td><td><a href="' + posturl + '">' + posturl + '</a></td></tr>';
 			  count = count + 1;
-		     }		   
-			});
+		     }
+		  }
 			html += '</tbody></table>';
 			if(count == 0){
 				html = '<h2>Not Found!</h2>'
-			}        
-	    }
-		$('#search').html('查询结果');	
-	    $('#result').html(html);
-		},      
-		error: function () {
-		$('#search').html('查询结果');	
-        $('#result').html('连接失败');
-     }
-
-  });
-});
+			}
+		
+		}
+		document.getElementById('result').innerHTML=html;
+	}else{
+		document.getElementById('result').innerHTML='No connection';
+	}
+  }
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+}
